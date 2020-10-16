5a359dd73263ccebba05302fa7872f0672f8140c
RoPalma95 <r.palma995@gmail.com>
2020-10-16 15:38:16 -0600

    Added floating point

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 const display = document.querySelector('.display');
 const numbers = [...document.querySelectorAll('.number')];
+const point = document.querySelector('.float-point');
 const operators = [...document.querySelectorAll('.operator')];
 const clear = document.querySelector('.clear');
 const equals = document.querySelector('.equals');
 let operator = '';
 let a = 0;
 let b = 0;
+let tracker = 1;
+
+display.textContent = 0;
 
 numbers.forEach(number => number.addEventListener('click', writeNumber));
-operators.forEach(operator => operator.addEventListener('click', writeOperator))
-clear.addEventListener('click', clearDisp)
+point.addEventListener('click', writeFloat);
+operators.forEach(operator => operator.addEventListener('click', getOperator))
+clear.addEventListener('click', clearVariables)
 equals.addEventListener('click', makeCalc)
 
 function writeNumber(e) {
+	if(!tracker){
+		display.textContent += e.target.textContent;
+	} else {
+		display.textContent = '';
+		display.textContent += e.target.textContent;
+		tracker = 0;
+	}
+}
+
+function writeFloat(e) {
 	display.textContent += e.target.textContent;
 }
 
@@ -20,23 +35,32 @@ function getNumber() {
 	return +display.textContent;
 }
 
-function writeOperator(e) {
-	a = getNumber();
-	clearDisp();
-	operator = e.target.textContent;
+function getOperator(e) {
+	if(a) {
+		b = getNumber();
+		a = operate(operator, a, b);
+		display.textContent = a;
+		operator = e.target.textContent;
+	} else {
+		a = getNumber();
+		operator = e.target.textContent;
+	}
+	tracker += 1; 
 }
 
-function clearDisp() {
-	display.textContent = '';
+function clearVariables() {
+	display.textContent = 0;
+	a = 0;
+	b = 0;
+	tracker += 1;
 }
 
 function makeCalc() {
 	b = getNumber();
-	clearDisp();
-
-	// console.log(a,b,operator);
-	let result = operate(operator, a, b);
-	display.textContent = result;
+	display.textContent = operate(operator, a, b);
+	a = 0;
+	b = 0;
+	tracker += 1;
 }
 
 const operations = {
@@ -50,6 +74,10 @@ const operations = {
 		return a * b;
 		},
 	'÷': function (a, b) {  //divition
+		if(b === 0) {
+			clearVariables();
+			return 'REALITY BROKEN';
+		}
 		return a / b;
 	},
 };
